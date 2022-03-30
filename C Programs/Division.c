8f48d584116725f38bd99a1618abbc78633df111
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-03-30 17:39:21 +0530

    arithmatic operation

---
+++ C Programs/Division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,div;
+	printf("Enter any two Numbers : ");
+	scanf("%d %d",&a,&b);
+	div=a/b;
+	printf("Division is = %d",div);
+}
---
+++ C Programs/Subtraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,mul;
+	printf("Enter any two Numbers : ");
+	scanf("%d %d",&a,&b);
+	mul=a*b;
+	printf("Multiplication is = %d",mul);
+}
