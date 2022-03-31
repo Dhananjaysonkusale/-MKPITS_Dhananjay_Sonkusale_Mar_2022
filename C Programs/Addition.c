85b63e605a0908dbb711b932e8a629227c23fb78
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-03-31 17:03:45 +0530

    Force Formula

--- C Programs/Addition.c
+++ C Programs/Addition.c
@@ -2,9 +2,5 @@
 #include<conio.h>
 void main()
 {
-	int a,b,sum;
-	printf("Enter any two Numbers");
-	scanf("%d %d",&a,&b);
-	sum=a+b;
-	printf("Addition is =%d",sum);
+	printf("Hello world");
 }
---
+++ C Programs/Force Formula.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float m,a,f;
+	printf("Enter the Mass: ");
+    scanf("%f", &m);
+    printf("Enter the Acceleration: ");
+    scanf("%f", &a);
+    f=m*a;
+    printf("The Force is : %f",f);
+    
+}
