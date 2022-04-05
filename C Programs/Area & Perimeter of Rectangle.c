2146ededffb4be7c4103a4b47518e06ed3b46863
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-05 20:38:35 +0530

    Area and Perimeter Of Rectangle

---
+++ C Programs/Area & Perimeter of Rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int w=5,h=7,a,p;
+	a=w*h;
+	printf("Area of Rectangle is %d \n",a);
+	
+	p=2*(h+w);
+	printf("Perimeter of Rectangle is %d",p);
+	
+}
