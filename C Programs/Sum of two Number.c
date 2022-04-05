f3aca8c2e7e8c0d4be0b2d3762f46c60ff2dd64f
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-05 20:41:06 +0530

    Sum of Two Number

---
+++ C Programs/Sum of two Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,sum;
+	printf("Enter number :");
+	scanf("%d",&a);
+	printf("Enter number :");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	printf("Sum = %d",sum);
+}
