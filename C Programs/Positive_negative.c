b7a5e20f6b836d1b5ee0761844ef2d1c22964738
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-08 00:22:09 +0530

    Check whether Number is Positive Or Negative

---
+++ C Programs/Positive_negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("Enter any number : ");
+	scanf("%d",&a);
+	
+	if(a>0)
+	{
+		printf("The Number is positive : ");
+	}
+	
+	else
+	{
+		printf("The Number is Negative : ");
+	}
+}
