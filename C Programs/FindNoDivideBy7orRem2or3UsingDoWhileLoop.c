5cd6c8c22cdc208009cbfaaa40153e8427183342
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-19 14:16:21 +0530

    Find Number divide by 7 or Rem 2 or 3 Using Do While Loop

---
+++ C Programs/FindNoDivideBy7orRem2or3UsingDoWhileLoop.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+	int a,b,t,i,sum=0;
+	
+    printf("Input the first integer: "); 
+    scanf("%d", &a);
+    printf("Input the second integer: ");
+    scanf("%d", &b);
+		
+	if(a>b) 
+    {
+		t=b;
+		b=a;
+		a=t;
+	}
+	i=a;
+	do
+	{
+		if((i%7)==2 || (i%7)==3)
+		{
+			printf("%d \n",i);
+		}
+		i=i+1;
+	}
+	while(i<=b);
+	
+	
+	
+}
