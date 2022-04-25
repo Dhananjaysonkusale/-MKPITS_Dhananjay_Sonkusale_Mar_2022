77d14104c41b9d1b45971fc1bb16a809cee96ea7
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-25 17:32:12 +0530

    Shift Data By 2 bit left_A6_52

---
+++ C Programs/ShiftDataBy2BitLeft_A6_52.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	
+	printf("Enter Any Number : ");
+	scanf("%d",&a);
+	
+	a<<=2;
+	
+	b=a;
+	
+	printf("Left shifted data = %d ",b);
+}
+
