d5f472a619fde74b107bd8ef4ff3d76eb9d26d71
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-26 14:49:47 +0530

    Calculate Square Cube Of Number_A7_68

---
+++ C Programs/Calculate_Square_CubeOfNumber_A7_68.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	printf("Print Number Square and Cube :\n");
+    for(num=0; num<=20; num++)
+        printf("%d\t%d\t%d\n",num,num*num,num*num*num);
+}
+
