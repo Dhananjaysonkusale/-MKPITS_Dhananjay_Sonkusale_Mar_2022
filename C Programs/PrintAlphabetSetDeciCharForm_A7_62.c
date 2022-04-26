20c48fbf7d3a51c21137a64d228521c271334bad
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-26 14:36:21 +0530

    Print Alphabet Set In Decimal and Char Form_A7_62

---
+++ C Programs/PrintAlphabetSetDeciCharForm_A7_62.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+  char c;
+  printf("Capital Letter Alphabet Set In Decimal : \n");
+  for (c=65;c<=90;c++) 
+  {
+    printf(" %c = %d \n ",c,c);
+  }
+
+  printf("Small Letters Alphabet Set In Decimal : \n");
+  for (c=97;c<=122;c++) 
+  {
+    printf(" %c = %d \n ",c,c);
+  }
+
+}
