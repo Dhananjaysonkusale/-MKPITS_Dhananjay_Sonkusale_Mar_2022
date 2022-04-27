cf739f8a5310b01f8eeab645871f16d481ef2db6
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-27 19:54:46 +0530

    Print Copied String

---
+++ C Programs/String_Copy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str1[20];
+	char str2[40]={"Dhananjay Yuvraj Sonkusale"};
+	
+	strcpy(str1,str2);
+	printf(" %s ",str1);
+}
