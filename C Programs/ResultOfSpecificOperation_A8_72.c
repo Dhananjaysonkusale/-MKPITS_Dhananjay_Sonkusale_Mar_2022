24940711f63c0b7f4626b2967cd623f67db15b90
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-28 22:59:28 +0530

    Print Result Of Specific Operation_A8_72

---
+++ C Programs/ResultOfSpecificOperation_A8_72.c
@@ -0,0 +1,12 @@
+//print result of specific operation 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d;
+	a=-1+4*6;
+	b=(35+5)%7;
+	c=14+(-4*6)/11;
+	d=2+15/6*1-7%2;
+	printf(" Result= %d\n Result= %d\n Result= %d\n Result= %d\n",a,b,c,d);
+}
