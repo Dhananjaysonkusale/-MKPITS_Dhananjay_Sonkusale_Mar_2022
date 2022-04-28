8fdce965121ec5e5538bbf9c92639e4eb1935c49
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-04-28 22:49:31 +0530

    Print Name By Using Function

---
+++ C Programs/Function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void disname();//to give information to compiler 
+void main()
+{
+	disname();
+	disname();
+	disname();
+}
+void disname()
+{
+	printf("Dhananjay ");
+}
