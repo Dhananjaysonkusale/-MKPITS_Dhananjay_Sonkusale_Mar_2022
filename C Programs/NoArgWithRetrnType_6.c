ab89282e337e10aa9e1dc7f2bc006873bf5bc9e7
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-05-04 10:41:30 +0530

    Function No Argument With Return Type_6

---
+++ C Programs/NoArgWithRetrnType_6.c
@@ -0,0 +1,35 @@
+//check whether the sequence of number 1 2 3 appears given array of integer 
+#include<stdio.h>
+#include<conio.h>
+int fun1()
+{
+	int i,a[5],t=0;
+	printf("Enter Five Number :\n");
+	
+	for(i=0;i<5;i++)
+	{
+		printf("Enter number :");
+		scanf("%d",&a[i]);
+		
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]==1 && a[i+1]==2 && a[i+2]==3)
+		{
+			t=1;
+		}
+	}
+	return t;
+}
+void main()
+{
+	int s=fun1();
+	if(s==1)
+	{
+	printf("\n Sequence 1 2 3 is appear in an array");	
+	}
+	else
+	{
+	printf("\n Sequence 1 2 3 is not appear in an array : ");	
+	}
+}
