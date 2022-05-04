327caf8daa0a7245ccc55748a3eceb5436d8d356
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-05-04 10:36:50 +0530

    Function No Argument No Return Type_8

---
+++ C Programs/NoArgNoRetType_8.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+#include<conio.h>
+void fun1()
+{
+	int a[6];
+	int i,t,p;
+	
+	for(i=0; i<6; i++) 
+	{
+    	printf("Enter Number : ");
+		scanf("%d", &a[i]);
+	}
+	t = a[0];
+	p = 0;
+	
+	for(i=0; i<6; i++) 
+	{
+		if(a[i]<t) 
+		{
+			t= a[i];
+			p = i;
+		}
+	}
+	
+	printf("Smallest Value: %d\n", t);
+	printf(" a[%d] = %d\n",a[t],t);
+}
+void main()
+{
+	fun1();
+}
