4c83bb40990c5225b84d8fc5fab8a1eaf2baac28
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-05-12 18:02:05 +0530

    Pattern Program_298

---
+++ C Programs/298.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include<conio.h>  
+void main()  
+{  
+    int a=1;  
+    
+    for(int i=3;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+          printf(" ");  
+        }  
+        for(int k=1;k<=a;k++)  
+        {  
+            printf("%d",a);  
+        }  
+        printf("\n");  
+        a++;  
+    }  
+}
