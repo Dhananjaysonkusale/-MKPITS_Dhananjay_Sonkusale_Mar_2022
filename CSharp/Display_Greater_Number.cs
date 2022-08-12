76fc184ea1f55d38487384f474b586ba1a2b9aed
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:32:25 +0530

    Display Greater Number

---
+++ CSharp/Display_Greater_Number.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Greater_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            Console.Write("Enter 1st number : ");
+            n1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number : ");
+            n2=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd number : ");
+            n3=Convert.ToInt32(Console.ReadLine());
+            if(n1>n2 && n1>n3)
+            {
+                Console.WriteLine("1st Number is Greater !");
+            }
+            else if(n2>n1 && n2>n3)
+            {
+                Console.WriteLine("2nd Number is Greater !");
+            }
+            else
+            {
+                Console.WriteLine("3rd Number is Greater !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
