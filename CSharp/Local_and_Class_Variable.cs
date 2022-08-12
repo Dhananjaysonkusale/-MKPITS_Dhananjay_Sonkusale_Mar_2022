73173464417665f0ce47a6b2ab7ac9ed378360f1
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:21:38 +0530

    Local and Class Variable

---
+++ CSharp/Local_and_Class_Variable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_and_Class_Variable
+{
+    internal class Program
+    {
+        static int gnum = 20;
+        static void display()
+        {
+            int num = 5;
+            Console.WriteLine("num inside method " + num);
+            Console.WriteLine("gnum inside method " + gnum);
+        }
+        static void Main(string[] args)
+        {
+            display();
+            // Console.WriteLine("num = " + num); error since num is local variable
+            Console.WriteLine("gnum inside main" + gnum);
+        }
+    }
+}
