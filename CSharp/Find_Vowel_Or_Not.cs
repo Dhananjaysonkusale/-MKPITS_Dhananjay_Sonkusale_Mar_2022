777d3c5763605ead4b8a86e71f860c5ea3cf3045
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:43:11 +0530

    Check Whether the Vowel Or Not

---
+++ CSharp/Find_Vowel_Or_Not.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Vowel_Or_Not_A1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Enter Letter : ");
+            a = Convert.ToChar(Console.ReadLine());
+
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o'|| a=='u')
+            {
+                Console.WriteLine("Enter Letter is Vowel !");
+            }
+            else if (a == 'A' || a == 'E' || a == 'I' || a == 'O'|| a=='U')
+            {
+                Console.WriteLine("Enter Letter is Vowel !");
+            }
+            else
+            {
+                Console.WriteLine("Enter Letter is Not Vowel");
+            }
+            Console.ReadLine();
+        }
+    }
+}
