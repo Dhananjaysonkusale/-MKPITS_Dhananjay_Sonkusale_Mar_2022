e7d44a65c9789f74be79ae736d6d60beeb1e746c
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:49:29 +0530

    Operator in C Sharp

---
+++ CSharp/Operator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2;
+            char op;
+            int res = 0;
+            Console.WriteLine("Enter 1st num ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd num ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operator");
+            op = Convert.ToChar(Console.ReadLine());
+            if (op == '+')
+            {
+                res = s1 + s2;
+
+            }
+            else if (op == '-')
+            {
+                res = s1 - s2;
+
+            } 
+            else if (op == '*')
+            {
+
+                res = s1 * s2;
+            }
+            else
+            {
+                Console.WriteLine("invalid operator");
+
+            }
+
+            Console.WriteLine("result = " + res);
+            Console.ReadLine();
+        }
+    }
+}
