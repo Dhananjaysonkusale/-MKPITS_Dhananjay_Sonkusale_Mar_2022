9eec05819899e4ca7edfd8ed3655f186a4ddcdf0
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:30:14 +0530

    Check Greater Number

---
+++ CSharp/Greater_No.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greater_No
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("Enter 1st Num ");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Num ");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a<b)
+            {
+                Console.WriteLine("2nd Number is Greater");
+            }
+            else
+            {
+                Console.WriteLine("1st Number is Greater");
+            }
+            Console.ReadLine();
+        }
+    }
+}
