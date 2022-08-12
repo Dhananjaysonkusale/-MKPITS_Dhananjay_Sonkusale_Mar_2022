f725aa9f8bd26581a3e909cfc578bc42390332b1
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:45:00 +0530

    Check Whether the Person is Eligible For Vote

---
+++ CSharp/Eligible_For_Vote_Or_Not.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_EligibleforVote_Or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter your Age = ");
+            age=Convert.ToInt32(Console.ReadLine());
+            if(age>=18)
+            {
+                Console.WriteLine("You Are Eligible For Vote");
+            }
+            else
+            {
+                Console.WriteLine("You Are Not Eligible For Vote");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
