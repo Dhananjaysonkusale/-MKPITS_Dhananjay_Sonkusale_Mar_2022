b212fdac238c0b9e3636cbb704958520c02cb073
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:28:55 +0530

    Division Of Number

---
+++ CSharp/Division_Of_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_Of_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2;
+
+            Console.WriteLine("Enter 1st Num1 : ");
+            num1=Convert.ToInt32(Console.ReadLine());   
+            Console.WriteLine("Enter 2nd Num2 : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int Result = num1 / num2;
+            Console.WriteLine("Result = " + Result);
+            Console.ReadLine();
+        }
+    }
+}
