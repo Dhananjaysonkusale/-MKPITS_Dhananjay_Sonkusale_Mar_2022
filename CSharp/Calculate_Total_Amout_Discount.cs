dd443b5a5d2f3ffb79bfc721b48e399aef60c03b
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:39:32 +0530

    Calculate Total Amount Of Product and Discount

---
+++ CSharp/Calculate_Total_Amout_Discount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Total_Amount_A1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pname;
+            int cost, quantity, tamount;
+            Console.Write("Enter Product Name : ");
+            pname = Console.ReadLine();
+            Console.Write("Enter Product Cost : ");
+            cost = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Quantity Of Product : ");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            tamount = cost * quantity;
+            Console.WriteLine("Total Amount = " + tamount);
+
+            if(tamount>2000)
+            {
+                float discount = tamount - (tamount * 0.5f);
+                Console.WriteLine("Total Amount After 50% Discount : "+discount);
+            }
+            else
+            {
+                float discount = tamount - (tamount * 0.1f);
+                Console.WriteLine("Total Amount After 10% Discount : " + discount);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
