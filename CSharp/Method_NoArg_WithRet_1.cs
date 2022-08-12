025caa1afedcf49f274dee3a5c9992e81bdc5e1f
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:18:51 +0530

    Method No Argument With Return Type1

---
+++ CSharp/Method_NoArg_WithRet_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_NoArg_NoRet_1
+{
+    
+        class Myprogram
+        {
+
+            static int square()
+            {
+                int num;
+                Console.WriteLine("Enter Any Number : ");
+                num = Convert.ToInt32(Console.ReadLine());
+                int sq = num * num;
+                return sq;
+           
+            }
+            static void Main(string[] args)
+            {
+
+                int r = square(); // calling the method display
+
+                Console.WriteLine("Square  =  "+ r);
+
+                Console.ReadLine();
+            }
+        }
+    
+}
