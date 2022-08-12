115bea9688ea73843a65faf312307ea8ea1caa42
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:11:48 +0530

    Array Using Foreach Loop

---
+++ CSharp/Array_Using_Foreach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Using_For_each_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            num[0] = 33;
+            num[1] = 22;
+            num[2] = 31;
+            num[3] = 54;
+            num[4] = 36;
+            //using foreach loop to iterate over the elements of an array
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
