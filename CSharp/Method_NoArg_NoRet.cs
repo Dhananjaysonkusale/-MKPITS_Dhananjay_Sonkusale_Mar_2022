a3c615c3f554d68f8e43a5a1756e95e2743cbe7f
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:17:20 +0530

    Method No Argument No Return

---
+++ CSharp/Method_NoArg_NoRet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_No_Arg_No_Return
+{
+    class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("hello from display method");
+        }
+        static void Main(string[] args)
+        {
+            display();
+            Console.WriteLine("bye");
+            Console.ReadLine();
+        }
+        
+    }
+}
