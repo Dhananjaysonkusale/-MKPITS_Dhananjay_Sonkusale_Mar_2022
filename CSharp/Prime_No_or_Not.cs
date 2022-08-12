14b47cbe7dcb23b94b1529ac4de1741c3873ba3b
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:06:52 +0530

    Check Whether The Number is Prime or Not

---
+++ CSharp/Prime_No_or_Not.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_No_or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int n, i, d = 0;
+
+			Console.WriteLine("Enter Any Number :");
+			n = Convert.ToInt32(Console.ReadLine());
+
+			i = 2;
+
+			while (i < n)
+			{
+				if (n % i == 0)
+				{
+					d = 1;
+					break;
+				}
+				i = i + 1;
+			}
+			if (d == 0)
+			{
+				Console.WriteLine("Enter Number is Prime !");
+			}
+			else
+			{
+				Console.WriteLine("Enter Number is Not Prime Number !");
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
