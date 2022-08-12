8086f790c9f5bdebd533aedf3523cfdd32d4fa9c
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:28:29 +0530

    Square Of Number

---
+++ CSharp/Square_Of_Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Of_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Any Number");
+            num=Convert.ToInt32(Console.ReadLine());
+            int Square = num * num;
+            Console.WriteLine("Square = "+Square);
+        }
+    }
+}
