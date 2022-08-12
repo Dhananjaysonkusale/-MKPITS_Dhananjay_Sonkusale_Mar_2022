50ea85951f70c5716a4bc71897dd1a93aa1a79c4
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:29:12 +0530

    Area Of Rectangle

---
+++ CSharp/Area_Of_Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_Of_Three_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2,num3 ,result;
+            Console.WriteLine("Enter 1st Number : ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd Number : ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            result = num1 + num2 + num3;
+            Console.WriteLine("Addition Of Three Number is = " + result);
+            Console.ReadLine();
+        }
+    }
+}
