aed326d296cd6263f436ea25db38c1a68c0f2993
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:59:44 +0530

    Print Table Of Number Using do While

---
+++ CSharp/Print_TableOfNumber_UsingdoWhile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_TableOfNo_Using_doWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int res = 1;
+            Console.Write("Enter Number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            do
+            {
+                res = num * i;
+                i++;
+                Console.WriteLine("{0} * {1} = {2}", num, i, res);
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
