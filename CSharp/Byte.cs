76f64cea93dd3e576b4dfd2ed78521c502a72961
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:31:30 +0530

    Byte In C Sharp

---
+++ CSharp/Byte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b1;
+            Console.WriteLine("enter bite ");
+            b1 = Convert.ToByte(Console.ReadLine());   //range of a byte is 0 to -255
+            Console.WriteLine("byte entered = " + b1);
+
+            Console.ReadLine();
+        }
+    }
+}
