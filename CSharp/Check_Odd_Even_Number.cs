683797f4b3affa84bbed3de9d306e4585844e363
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 22:44:07 +0530

    Check Odd Even Number

---
+++ CSharp/Check_Odd_Even_Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_No_Odd_Or_Even_A1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("Enter Any Number : ");
+            n1=Convert.ToInt32(Console.ReadLine());
+
+            if(n1%2==0)
+            {
+                Console.WriteLine("Enter Number is Even ");
+            }
+            else
+            {
+                Console.WriteLine("Enter Number is Odd ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
