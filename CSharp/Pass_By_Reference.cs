0b786970c52612a7431678eeb8ae38d2a8959f19
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:23:21 +0530

    Pass By Reference

---
+++ CSharp/Pass_By_Reference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_By_Reference
+{
+    internal class Program
+    {
+        static void display(ref int num)
+        {
+            num++;
+            Console.WriteLine("Num inside method = " + num); //6
+
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 5;
+            display(ref num);
+
+            Console.WriteLine("Num inside main = " + num); //6
+        }
+    }
+}
