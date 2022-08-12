f6d0fe740a1921395fa6f0bce708282f40db83f2
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:15:59 +0530

    Jump Statement 2

---
+++ CSharp/Jump_Statement2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jump_Statement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Any Number : ");
+            num= Convert.ToInt32(Console.ReadLine());
+
+
+            if (num % 2 == 0) goto even;
+            Console.WriteLine("ODD");
+            goto end;
+
+        even:
+            Console.WriteLine("No is Even");
+            goto end;
+        end:
+            Console.WriteLine("THE END");
+            Console.ReadLine();
+        }
+        
+    }
+}
