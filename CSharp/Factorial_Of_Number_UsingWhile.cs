636b330dfaa7c3b3da6e3cfcb0297b8b807d3c33
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:03:27 +0530

    Factorial Of Number Using While

---
+++ CSharp/Factorial_Of_Number_UsingWhile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Of_Number_UsingWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int fact = 1;
+            Console.Write("Enter Any Number : ");
+            n=Convert.ToInt32(Console.ReadLine());
+            int i = n;
+            while ( i >0 )
+            {
+                fact = fact*i;
+                i--;
+            }
+            Console.WriteLine("Factorial = " + fact);
+            Console.ReadLine();
+        }
+    }
+}
