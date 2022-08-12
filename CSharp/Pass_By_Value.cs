81763ef639ca18a24fec826fa029faf2ed0c23eb
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:22:50 +0530

    Pass By Value

---
+++ CSharp/Pass_By_Value.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_by_Value
+{
+    internal class Program
+    {
+        static void display(int num)
+        {
+            num++;
+            Console.WriteLine("Num inside method = " + num);
+
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Any Number");
+            num = Convert.ToInt32(Console.ReadLine());
+            display(num);
+
+            Console.WriteLine("Num inside main = " + num);
+        }
+    }
+}
