495f2cac743f58318eb517451bc3f1baaf116322
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:00:17 +0530

    Factorial Of Number

---
+++ CSharp/Factorial_Of_Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Of_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int fact = 1;
+            Console.WriteLine("Enter Any Number : ");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = num; i > 0; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial = " + fact);
+            Console.ReadLine();
+        }
+    }
+}
