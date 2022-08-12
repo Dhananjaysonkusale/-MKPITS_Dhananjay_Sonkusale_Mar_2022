d6d839a11ebdb73129b35a998dd8ba7cfee09491
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-12 23:13:32 +0530

    String Using Foreach Loop

---
+++ CSharp/String_Foreach_Loop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_For_each_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //example of foreach loop
+            //create an array of 5 element
+            string[] num = new string[8];
+
+            num[0] = "Dhananjay";
+            num[1] = "Akshay";
+            num[2] = "Krunal";
+            num[3] = "Shubham";
+            num[4] = "Tanmay";
+            num[5] = "Aniket";
+            num[6] = "Amit";
+            num[7] = "Nandu";
+            //using foreach loop to iterate over the elements of an array
+            foreach (string i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
