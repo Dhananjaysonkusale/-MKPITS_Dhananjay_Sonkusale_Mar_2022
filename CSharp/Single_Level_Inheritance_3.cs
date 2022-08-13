3f1b23ab0e5fc5f42e44d14ce571fddfc043885e
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 10:05:17 +0530

    Single Level Inheritance_3

---
+++ CSharp/Single_Level_Inheritance_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Single_3
+{
+    public class Animal
+    {
+        public void eat()
+        { 
+            Console.WriteLine("Eating"); 
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark() 
+        { 
+            Console.WriteLine("Barking"); 
+        }
+    }
+    class TestInheritance2
+    {
+        public static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.eat();
+            d1.bark();
+        }
+    }
+}
+
