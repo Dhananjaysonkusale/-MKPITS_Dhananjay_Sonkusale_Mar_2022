4c0eb48b48eeff7d49f9d65e26884de36ae85d1b
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:40:56 +0530

    Create Class Customer Print Detail_A5

---
+++ CSharp/Class_Customer_A4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Customer_A4
+{
+    class Customer
+    {
+        public string custname;
+        public int custno;
+        public string custadd;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer cust = new Customer();
+            Console.WriteLine("Enter Your Name : ");
+            cust.custname = Console.ReadLine();
+            Console.WriteLine("Enter Your Address : ");
+            cust.custadd = Console.ReadLine();
+            Console.WriteLine("Enter Your Number : ");
+            cust.custno=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Customer Name : "+cust.custname);
+            Console.WriteLine("Customer Address : "+cust.custadd);
+            Console.WriteLine("Customer Number : "+cust.custno);
+
+        }
+    }
+}
