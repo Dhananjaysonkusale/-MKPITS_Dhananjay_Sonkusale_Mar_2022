b41035a0cc12a3a5c27cf322b5079c7c1741d3cd
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:47:46 +0530

    Create Constructor Of Class Product

---
+++ CSharp/Constructor_Class_Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Class_Product
+{
+    class Product
+    {
+        string productname;
+        int productid;
+        int productprice;
+        int productquant;
+
+        public Product()
+        {
+            Console.WriteLine("Enter Product Name : ");
+            productname = Console.ReadLine();
+            Console.WriteLine("Enter Product ID : ");
+            productid=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Price : ");
+            productprice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Quantity : ");
+            productquant = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void display()
+        {
+            Console.WriteLine("Product Name : "+productname);
+            Console.WriteLine("Product Id : "+productid);
+            Console.WriteLine("Product Price : "+productprice);
+            Console.WriteLine("Product Quantity : "+productquant);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(); //Object Of Constructor Product
+            p.display();
+            Console.ReadLine();
+
+        }
+    }
+}
