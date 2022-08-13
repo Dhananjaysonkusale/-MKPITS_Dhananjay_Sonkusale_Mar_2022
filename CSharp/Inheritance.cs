75795100f9e5fb9a0a0776a19e4060264d73299e
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:53:32 +0530

    Inheritance

---
+++ CSharp/Inheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Inheritance
+{
+    //creating a base class person
+    class person
+    {
+        public string name;
+    }
+    //creating a derieved class student
+    class student : person
+    {
+        public int rno;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.rno = 34;
+            s.name = "Dhananjay";
+            Console.WriteLine("Roll No : " + s.rno);
+            Console.WriteLine("Name : " + s.name);
+        }
+    }
+}
