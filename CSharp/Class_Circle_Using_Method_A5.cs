9e60152b4d3259bdc2ec3f4bd3a7abe9280cd7d6
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:44:49 +0530

    Class Circle Using Method_A5

---
+++ CSharp/Class_Circle_Using_Method_A5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Circle_Using_Method_A5
+{
+    class Circle
+    {
+        public float radius;
+
+        public void area(float r)
+        {
+            float area = 3.13f * r * r;
+            Console.WriteLine("Area = "+area);
+        }
+        public void circ(float r)
+        {
+            float circum = 2 * 3.14f * r;
+            Console.WriteLine("Circumference = " + circum);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cir = new Circle();
+            Console.WriteLine("Enter Radius : ");
+            float radius =Convert.ToSingle(Console.ReadLine());
+
+            cir.area(radius);
+            cir.circ(radius);
+        }
+    }
+}
