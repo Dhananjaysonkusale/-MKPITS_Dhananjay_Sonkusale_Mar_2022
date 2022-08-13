702578b05a11f8917d9d705a1f8d3db6a494d4e6
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:53:13 +0530

    Access Modifier Private

---
+++ CSharp/Access_Modifier_Private.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifier_Private
+{
+    class student
+    {
+        protected string name;
+        public void getdata()
+        {
+            name = "Dhananjay";
+            Console.WriteLine("Name : " + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            //s.name = "Dhananjay";
+            s.getdata();
+        }
+
+    }
+}
