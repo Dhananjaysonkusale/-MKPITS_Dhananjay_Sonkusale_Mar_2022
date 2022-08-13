5d2d70b4bf8d80be689d12b71bc4a8ff84af9b4f
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:39:31 +0530

    Class Object Getdata Showdata Method

---
+++ CSharp/Class_Object_Getdata_Showdata.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_getdata_Showdata
+{
+    class Employee
+    {
+        public int empid;
+        public string empname;
+        public int empsal;
+
+        public void getdata()
+        {
+            Console.WriteLine("Enter Employee ID : ");
+            empid=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name : ");
+            empname=Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Employee Sallary : ");
+            empsal=Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void showdata()
+        {
+            Console.WriteLine("Employee ID : "+empid);
+            Console.WriteLine("Employee ID : "+empname);
+            Console.WriteLine("Employee ID : "+empsal);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an object(instance) of employee class
+            //syntax classname objectname =new classname();
+            Employee emp = new Employee();
+            emp.getdata();
+            emp.showdata();
+            Console.ReadLine();
+        }
+    }
+}
