f1d853d99afc36457a5bf5fba1519052e61b7cdf
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:29:56 +0530

    Print No Using Loop

---
+++ CSharp/Print_No_Using_Loop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Print_No_Using_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("No is ");
+
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
