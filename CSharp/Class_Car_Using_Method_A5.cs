bfafe68f8f7a61b3b0db873f644ef792d4b3464c
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:42:57 +0530

    Create Class Car Using Method_A5

---
+++ CSharp/Class_Car_Using_Method_A5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Car_Using_Method_A5
+{
+    public class Car
+    {
+        string carname;
+        string carmodel;
+       
+        public void getdata(string a, string b)
+        {
+            carname = a;
+            carmodel = b;
+        }   
+        public void showdata()
+        {
+            Console.WriteLine("Car Name : " + carname);
+            Console.WriteLine("Car Model : " + carmodel);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            Console.WriteLine("Enter Car Name : ");
+            string carname = Console.ReadLine();
+            Console.WriteLine("Enter Car Model : ");
+            string carmodel = Console.ReadLine();
+
+
+            car.getdata(carname, carmodel); ;
+            car.showdata();
+
+        }
+    }
+}
