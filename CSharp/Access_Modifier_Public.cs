a7eaa71e153395f3b543f72aafb396d9536d9d28
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-13 09:52:55 +0530

    Access Modifier Public

---
+++ CSharp/Access_Modifier_Public.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifier_Public
+{
+    class student
+    {
+        class PublicTest
+        {
+            public string name = "Dhananjay Sonkusale";
+            public void Msg(string msg)
+            {
+                Console.WriteLine("Hello " + msg);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                PublicTest p = new PublicTest();
+                // Accessing public variable
+                Console.WriteLine("Hello " + p.name);
+                // Accessing public function
+
+                p.Msg("Akshay Pawar ");
+            }
+        }
+    }
+}
