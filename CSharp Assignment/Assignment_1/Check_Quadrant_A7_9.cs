7bb48d89ad3c988644a7a93bd4cc57fb6b031ea8
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 20:42:57 +0530

    Check_Quadrant_A7_9

---
+++ CSharp Assignment/Assignment_1/Check_Quadrant_A7_9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Quadrant_A7_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Enter x Coordinate : ");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter y Coordinate : ");
+            y=Convert.ToInt32(Console.ReadLine());
+            if(x>0 && y>0)
+            {
+                Console.WriteLine("Point ({0},{1}) lies in the First quadrant.", x, y);
+            }
+            else if(x<0 && y>0)
+            {
+                Console.WriteLine("Point ({0},{1}) lies in the Second quadrant.", x, y);
+            }
+            else if(x<0 && y<0)
+            {
+                Console.WriteLine("Point ({0},{1}) lies in the Third quadrant.", x, y);
+            }
+            else if(x>0 && y<0)
+            {
+                Console.WriteLine("Point ({0},{1}) lies in the Fourth quadrant.", x, y);
+            }
+        }
+    }
+}
