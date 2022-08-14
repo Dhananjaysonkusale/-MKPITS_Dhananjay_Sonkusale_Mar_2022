a15c1c35305996df31d2a8869d2b1cf4e2883f00
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-08-14 20:38:33 +0530

    Check_Number_Is_Odd_or_Not_A7_2

---
+++ CSharp Assignment/Assignment_1/Check_Number_Is_Odd_or_Not_A7_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Number_Is_Odd_Or_Not_A7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Any Number : ");
+            num=Convert.ToInt32(Console.ReadLine());
+            if(num%2 != 0)
+            {
+                Console.WriteLine("{0} Number is Odd ",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} Number is Even", num);
+            }
+        }
+    }
+}
