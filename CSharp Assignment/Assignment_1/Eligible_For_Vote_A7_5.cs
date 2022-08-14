eaf0b32184d9035605c7bc3d81a99feb2b638480
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 20:40:33 +0530

    Eligible_For_Vote_A7_5

---
+++ CSharp Assignment/Assignment_1/Eligible_For_Vote_A7_5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eligible_For_Vote_A7_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Enter Your Age : ");
+            age=Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Congratulation! You are eligible for casting your vote");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! You are Not eligible for casting your vote");
+            }
+        }
+    }
+}
