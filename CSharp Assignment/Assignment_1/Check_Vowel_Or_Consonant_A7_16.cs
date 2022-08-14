296af131f18ea6593d00c612dd2ce3437fad2c52
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 22:11:31 +0530

    Check_Vowel_Or_Consonant_A7_16

---
+++ CSharp Assignment/Assignment_1/Check_Vowel_Or_Consonant_A7_16.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Vowel_Or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Enter Any Alphabet : ");
+            a = Convert.ToChar(Console.ReadLine());
+
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+            {
+                Console.WriteLine("Entered Alphabet is Vowel !");
+            }
+            else if (a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
+            {
+                Console.WriteLine("Entered Alphabet is Vowel !");
+            }
+            else
+            {
+                Console.WriteLine("Entered Alphabet is Consonant ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
