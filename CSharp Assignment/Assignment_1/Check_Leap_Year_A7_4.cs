c8f0445d7a1d736d5b3e05315514a288e1509c56
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 20:40:09 +0530

    Check_Leap_Year_A7_4

---
+++ CSharp Assignment/Assignment_1/Check_Leap_Year_A7_4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Leap_Year_A7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Enter Year : ");
+            year=Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("{0} is a Leap Year ",year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Not a Leap Year ",year);
+
+            }
+        }
+    }
+}
