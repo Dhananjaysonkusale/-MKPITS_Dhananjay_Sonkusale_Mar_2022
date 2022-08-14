9b333ed6efb02382c7c285679c4a661241bce5fa
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 22:11:08 +0530

    Check_Triangle_Formed_A7_15

---
+++ CSharp Assignment/Assignment_1/Check_Triangle_Formed_A7_15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Triangle_Formed_A7_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,total;
+            Console.Write("Enter 1st Side Of Triangle : ");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd Side Of Triangle : ");
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd Side Of Triangle : ");
+            c=Convert.ToInt32(Console.ReadLine());
+
+            total = a + b + c;
+            if(total == 180)
+            {
+                Console.WriteLine("Triangle Can Be Formed ");
+            }
+            else
+            {
+                Console.WriteLine("Triangle Can Not Formed ");
+            }
+        }
+    }
+}
