1fad42c4e612c2ec6d8e52d83c66b377d959e39b
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 20:41:14 +0530

    Check_Height_Of_Person_A7_7

---
+++ CSharp Assignment/Assignment_1/Check_Height_Of_Person_A7_7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Height_Of_Person_A7_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            Console.Write("Enter Height in cm : ");
+            height=Convert.ToInt32(Console.ReadLine());
+            if (height < 150)
+            {
+                Console.WriteLine("The Person is Dwarf");
+            }
+            else if(height >= 150 && height < 165)
+            {
+                Console.WriteLine("Average Height");
+            }
+            else if(height>=165)
+            {
+                Console.WriteLine("The Person is Tall");
+            }
+        }
+    }
+}
