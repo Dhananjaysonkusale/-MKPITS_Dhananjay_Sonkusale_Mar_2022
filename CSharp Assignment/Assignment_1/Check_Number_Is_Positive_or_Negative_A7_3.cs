4f158e4629bc932ad5426aa544e959c3c1c05215
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 20:39:30 +0530

    Check_Number_Is_Positive_or_Negative_A7_3

---
+++ CSharp Assignment/Assignment_1/Check_Number_Is_Positive_or_Negative_A7_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Number_Is_Positive_or_Negative_A7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Any Number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("{0} is a Positive Number ",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a Negative Number ",num);
+            }
+        }
+    }
+}
