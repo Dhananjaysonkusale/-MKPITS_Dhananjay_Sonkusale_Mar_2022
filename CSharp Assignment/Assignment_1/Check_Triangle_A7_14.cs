1716a54faeafc8d5e6635c7fdbe47ab3c9be706f
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-14 22:10:50 +0530

    Check_Triangle_A7_14

---
+++ CSharp Assignment/Assignment_1/Check_Triangle_A7_14.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Triangle_A7_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            Console.Write("Enter 1st Side Of Triangle : ");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 2nd Side Of Triangle : ");
+            b=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 3rd Side Of Triangle : ");
+            c=Convert.ToInt32(Console.ReadLine());
+
+            if(a==b && b==c)
+            {
+                Console.WriteLine("This is an equilateral triangle ");
+            }
+            else if(a==b || a==c|| b==c)
+            {
+                Console.WriteLine("This is an isosceles triangle ");
+            }
+            else
+            {
+                Console.WriteLine("This is a scalene triangle");
+            }
+
+        }
+    }
+}
