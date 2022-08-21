895c30457215416fa778b704f375a508c4aa7e18
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:17:52 +0530

    Encapsulation_Ex_1

---
+++ CSharp/Encapsulation_Ex_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Ex_1
+{
+    internal class Program
+    {
+        class employee
+        {
+            // string empname;
+            public string empname { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            employee emp = new employee();
+            emp.empname = " Dhananjay "; //this will call setter
+            Console.WriteLine("Employee Name : " + emp.empname);
+        }
+    }
+}
