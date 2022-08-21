21aba80736b4757ffd5beaad1faaceda3cadadde
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:56:04 +0530

    Factorial_Of_Number_A8_15

---
+++ CSharp Assignment/Assignment_2/Factorial_Of_Number_A8_15.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Of_Number_A8_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,fact=1;
+            Console.Write("Enter Number : ");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = n; i > 0; i--)
+            {
+                fact=fact*i;
+            }
+            Console.WriteLine("Factorial = "+fact);
+        }
+    }
+}
