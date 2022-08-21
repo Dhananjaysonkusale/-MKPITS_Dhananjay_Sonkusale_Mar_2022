716059a9c2ca43d0c06694c9fe26d20b4dfd2cff
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:57:41 +0530

    Copy_Array_into_Another_A9_4

---
+++ CSharp Assignment/Assignment_3/Copy_Array_into_Another_A9_4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Copy_Array_into_Another_A9_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] a = new int[100];
+            int [] b = new int[100];
+            int n;
+            Console.Write("Enter Number of Array Element :  ");
+            n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Enter Array Element {0} : ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine());                
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = a[i];
+            }
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Element In 1st Array : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("a[{0}] : {1}", i, a[i]);
+                
+            }
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Element In 2nd Array : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("a[{0}] : {1}", i, b[i]);                
+            }
+            Console.WriteLine("--------------------END-----------------------");
+            Console.ReadLine();
+        }
+    }
+}
