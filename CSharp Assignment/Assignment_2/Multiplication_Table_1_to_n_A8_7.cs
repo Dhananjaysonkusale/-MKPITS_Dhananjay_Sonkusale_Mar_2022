2cd8581077f09d61f9fb6dc72e5ea6354d3175ba
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:51:47 +0530

    Multiplication_Table_1_to_n_A8_7

---
+++ CSharp Assignment/Assignment_2/Multiplication_Table_1_to_n_A8_7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table_1_to_n_A8_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i, num;
+           
+            Console.Write("Input upto the table number starting from 1 : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Multiplication table from 1 to {0} ", num);
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= num; j++)
+                {
+                    if (j <= num - 1)
+                    {
+                        Console.Write("{0}x{1} = {2}, ", j, i, i * j);
+                    }
+                    else
+                    {
+                        Console.Write("{0}x{1} = {2}", j, i, i * j);
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
