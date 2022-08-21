4a6dc8cfc6a9508228f7a3327627ef456ae607d2
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:15:46 +0530

    Polymorphism_Class_Acoount_Ex_4

---
+++ CSharp/Polymorphism_Class_Acoount_Ex_4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Class_Acoount_Ex_4
+{
+    internal class Program
+    {
+        class Account
+        {
+            public int AccNo;
+            public int Balance=5000;
+
+            public string deposite (int AccNo,int Amount)
+            {
+                return "Deposit method of account class";
+            }
+            public string showbalance()
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
