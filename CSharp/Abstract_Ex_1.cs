fca4f86b93555f2d2af641154795ae173c1111dd
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:18:55 +0530

    Abstract_Ex_1

---
+++ CSharp/Abstract_Ex_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Ex_1
+{
+    abstract class account
+    {
+        public abstract void deposit();
+
+    }
+    class saving : account
+    {
+        public override void deposit()
+        {
+            Console.WriteLine("Hello from saving depsoit method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // account act = new account(); //error
+            account act = new saving();
+            act.deposit();
+
+        }
+
+    }
+}
