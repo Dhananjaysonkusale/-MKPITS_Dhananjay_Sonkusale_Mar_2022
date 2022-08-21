6f4c68711bc2200de27fbf73611a66478720828f
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:14:02 +0530

    Base_Class_Construction_Internally_2

---
+++ CSharp/Base_Class_Construction_Internally_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Class_Construction_Internally_2
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal");
+        }
+    }
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("Dog");
+        }
+
+    }
+    public class TestOverriding
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+
+        }
+    }
+}
