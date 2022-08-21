83aa1ea59486f7ae2740e949e90949d0a3ff9adf
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:53:42 +0530

    Print_Pattern_Right_Angle_Triangle_A8_9

---
+++ CSharp Assignment/Assignment_2/Print_Pattern_Right_Angle_Triangle_A8_9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Pattern_Right_Angle_Triangle_A8_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, i, j;
+            Console.Write("Enter Number of rows : ");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= r; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
