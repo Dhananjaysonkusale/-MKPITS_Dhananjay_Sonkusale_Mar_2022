d62e08f3f8d08abdf352ae70ca1277963062d89b
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-08-21 13:45:08 +0530

    Display Number Bet 1 to 10

---
+++ CSharp Assignment/Assignment_2/Display_Number_1to10_A8_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Number_1to10_A8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Number From 1 to 10 : ");
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(" " + i);
+                //Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
