f0821f080c23fbfa06a5c5921b26c8f29394813c
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:54:25 +0530

    Print_Number_Pattern_RepeatNum_A8_11

---
+++ CSharp Assignment/Assignment_2/Print_Number_Pattern_RepeatNum_A8_11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Number_Pattern_RepeatNum_A8_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, i, j;
+            Console.Write("Enter Number of rows : ");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= r; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ",i);                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
