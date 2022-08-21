17ba27c51ca75cb3ba807fa0500e8435ad6b7bd9
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:14:20 +0530

    Polymorphism_Runtime

---
+++ CSharp/Polymorphism_Runtime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Runtime
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public virtual void dance()
+            {
+                Console.WriteLine("Dancing");
+            }
+        }
+        public class Boys : Person
+        {
+            public override void dance()
+            {
+                Console.WriteLine("Boys Are Dancing");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Boys();
+            p.dance();
+            
+        }
+    }
+}
