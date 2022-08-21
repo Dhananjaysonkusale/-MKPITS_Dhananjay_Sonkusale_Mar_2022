a31b5887624cc1b684262c90123ba8405b3182b4
Dhananjaysonkusale <dhananjaysonkusale@gmail.com>
2022-08-21 13:47:16 +0530

    Array to Print Number

---
+++ CSharp Assignment/Assignment_3/Array_To_Print_Number_A9_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_To_Print_Number_A9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            Console.WriteLine("Enter 10 Element in Array : ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter Array Element {0} : ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Element in Array : ");
+            for (int i=0; i < 10; i++)
+            {
+                Console.WriteLine("a[{0}] = {1} ",i, a[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
