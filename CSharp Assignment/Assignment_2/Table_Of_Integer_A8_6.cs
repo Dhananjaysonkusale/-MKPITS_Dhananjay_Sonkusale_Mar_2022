cfb5714d14b555642074a3106e6440dab559d68e
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:51:00 +0530

    Table_Of_Integer_A8_6

---
+++ CSharp Assignment/Assignment_2/Table_Of_Integer_A8_6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_Of_Integer_A8_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int res = 1;
+            Console.Write("Enter A Number : ");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= 10; i++)
+            {
+                res = n * i;
+                Console.WriteLine("{0}*{1} ={2}",n,i,res);
+            }
+        }
+    }
+}
