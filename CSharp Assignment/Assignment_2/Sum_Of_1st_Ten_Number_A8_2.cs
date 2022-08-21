60ca74d6a874215bdbcead4c9ff5cb6ebe26bf40
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:48:43 +0530

    Sum_Of_1st_Ten_Number_A8_2

---
+++ CSharp Assignment/Assignment_2/Sum_Of_1st_Ten_Number_A8_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Of_1st_Ten_Number_A8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("1st 10 Natural Number : ");
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i <=10 ; i++)
+            {
+                sum= sum + i;               
+            }
+            Console.WriteLine("Sum of 1st 10 Number is = {0} ",sum);
+        }
+    }
+}
