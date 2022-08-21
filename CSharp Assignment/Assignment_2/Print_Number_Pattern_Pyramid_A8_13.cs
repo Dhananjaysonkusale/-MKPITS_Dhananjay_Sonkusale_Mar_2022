bd8d4a934323b57ef83d5068f750b8958d4bdda9
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:55:21 +0530

    Print_Number_Pattern_Pyramid_A8_13

---
+++ CSharp Assignment/Assignment_2/Print_Number_Pattern_Pyramid_A8_13.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Number_Pattern_Pyramid_A8_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, i, j;
+            Console.Write("Enter Number of rows : ");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= r; i++)
+            {
+                for (j = r; j >=i; j--)
+                {
+                    Console.Write(" ");                    
+                }
+                int count = 1;
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(count + " ");
+                    count=count+1;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
