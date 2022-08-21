6ec95b7dc1af93f7e35213f6407fcef0c1dfa595
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:13:30 +0530

    Base Keyword 2

---
+++ CSharp/Base_Keyword_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Keyword_2
+{
+    internal class Program
+    {
+        public class Human
+        {
+            public void Dance()
+            {
+
+                Console.WriteLine("Boys Are Dancing ");
+            }
+        }
+
+        public class Gender : Human
+        {
+            public void Dance()
+            {
+                base.Dance(); //this will class base class method eat
+                Console.WriteLine("Girls Are Dancing");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Gender g = new Gender();
+            g.Dance();
+        }
+    }
+}
