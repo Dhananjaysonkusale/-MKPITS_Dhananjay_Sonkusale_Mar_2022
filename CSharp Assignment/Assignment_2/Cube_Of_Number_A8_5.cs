957b9a7afc41fd98fb648f2e165ccf5d2ae1e7b1
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:50:00 +0530

    Cube_Of_Number_A8_5

---
+++ CSharp Assignment/Assignment_2/Cube_Of_Number_A8_5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube_Of_Number_A8_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n;
+            
+            Console.WriteLine("Input number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Number is : {0} and cube of the {1} is :{2} ",i,i,(i * i * i));
+            }
+        }
+    }
+}
