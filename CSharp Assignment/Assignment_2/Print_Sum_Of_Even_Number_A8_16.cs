17617f7eaed32a6b9dafbdb96b14c84fef636f44
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:56:27 +0530

    Print_Sum_Of_Even_Number_A8_16

---
+++ CSharp Assignment/Assignment_2/Print_Sum_Of_Even_Number_A8_16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Sum_Of_Even_Number_A8_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Console.Write("Enter Any Number  = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int a = 2 * i;
+                Console.WriteLine("{0}", a);
+                sum = sum + a;
+            }
+            Console.WriteLine("Sum = " + sum);
+        }
+    }
+}
