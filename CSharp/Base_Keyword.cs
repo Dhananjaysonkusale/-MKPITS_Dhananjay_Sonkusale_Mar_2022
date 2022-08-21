d4e9073455877227554aa288f763bb46ae428ae1
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:12:33 +0530

    Base Keyword

---
+++ CSharp/Base_Keyword.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public string color = "White";
+        }
+        public class Dog : Animal
+        {
+            string color = "Black";
+            public void showColor()
+            {
+                Console.WriteLine(base.color);
+                Console.WriteLine(color);
+            }
+
+        }
+        public class TestBase
+        {
+            public static void Main()
+            {
+                Dog d = new Dog();
+                d.showColor();
+            }
+        }
+    }
+}
