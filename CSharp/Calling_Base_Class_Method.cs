4eed6df918091686376d3a6115a1af98d59b8c3c
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:13:01 +0530

    Calling_Base_Class_Method

---
+++ CSharp/Calling_Base_Class_Method.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Base_Class_Method
+{
+        class person
+        {
+            string name = "MKPITS";
+            public void display()
+            {
+                Console.WriteLine(" base class field name = " + name);
+            }
+
+        }
+        class student : person
+        {
+            string name = "Dhananjay";
+            public void display()
+            {
+                //calling base class display method
+                base.display();
+                Console.WriteLine(" child class field name = " + name);
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                student s = new student();
+                s.display();
+            }
+        }
+            
+}
