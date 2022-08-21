bec5c1d6713f24fa8f91863a14cf6fa57b9743f4
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:15:22 +0530

    Polymorphism_Ex_3

---
+++ CSharp/Polymorphism_Ex_3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Ex_3
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string name = "Dhananjay";
+        }
+        class Employee : Person
+        {
+            public string name = "Akshay";
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Employee();
+
+            Console.WriteLine(p.name);
+            
+        }
+    }
+}
