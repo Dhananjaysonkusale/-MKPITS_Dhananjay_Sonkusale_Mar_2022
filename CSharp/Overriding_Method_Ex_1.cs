b878ba0ee876575c51640b1f2106cdd155ccb7bc
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 14:18:28 +0530

    Overriding_Method_Ex_1

---
+++ CSharp/Overriding_Method_Ex_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding_Method_Ex_1
+{
+    internal class Program
+    {
+        public class School
+        {
+            public virtual void Learning()
+            {
+                Console.WriteLine("School ");
+            }
+        }
+        public class Student : School
+        {
+            public override void Learning()
+            {
+                Console.WriteLine("Student Learning");
+            }
+        }
+        static void Main(string[] args)
+        {
+            School d = new Student();
+            d.Learning();
+        }
+    }
+}
