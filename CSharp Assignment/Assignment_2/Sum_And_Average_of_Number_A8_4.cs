e05ceed628911875b415faab7a0c460010074cb9
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-21 13:49:38 +0530

    Sum_And_Average_of_Number_A8_4

---
+++ CSharp Assignment/Assignment_2/Sum_And_Average_of_Number_A8_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_And_Average_of_Number_A8_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            float avg;
+            Console.WriteLine("Enter Any 10 Number : ");
+            
+            for(int i = 1; i <=10; i++)
+            {
+                Console.Write("Enter {0} Number : ", i);
+                n=Convert.ToInt32(Console.ReadLine());
+                sum = sum + n;
+            }
+            Console.WriteLine("The Sum Of 10 Number is  = " +  sum);
+
+            avg = sum / 10.0f;
+            Console.WriteLine("Average = {0}",avg);
+        }
+    }
+}
