1157e2d66c1dadd6d696a4c9f818b8e2dbd988a9
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 19:13:55 +0530

    Exception_Handling_Ex_13

---
+++ CSharp/Exception_Handling_Ex_13.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Ex_13
+{
+    internal class Program
+    {
+        class ageexception : ApplicationException
+        {
+            public ageexception(string message) : base(message)
+            {
+            }
+        }
+        class age
+        {
+            public int age1;
+            public void checkage(int age1)
+            {
+                this.age1 = age1;
+                if (age1 < 18)
+                {
+                    throw (new ageexception("You are not elegible to vote"));
+                }
+                else
+                {
+                    Console.WriteLine("You are eligible to vote ");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            age a = new age();
+            Console.WriteLine("Enter your age : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                a.checkage(num);
+            }
+            catch (ageexception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Bye ");
+        }
+    }
+}
