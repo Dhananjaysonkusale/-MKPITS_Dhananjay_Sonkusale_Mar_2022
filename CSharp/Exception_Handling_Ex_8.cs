9d69e9e236983fbd20d76ab7e77264b46ebd741e
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 19:04:34 +0530

    Exception_Handling_Ex_8

---
+++ CSharp/Exception_Handling_Ex_8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Ex_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            try
+            {
+                Console.WriteLine("Enter Age : ");
+                age = Convert.ToInt32(Console.ReadLine());
+                //if the user enters age below 18 then throw the exception
+                if (age < 18)
+{
+                    throw new Exception("Age should be greater or equal to 18 ");
+                }
+            }
+            catch (OverflowException ee)
+            {
+                Console.WriteLine(ee.ToString());
+                Console.WriteLine("Enter a small number please ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+                Console.WriteLine("Enter age 18 or greater than 18 ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            finally
+            {
+                Console.WriteLine("finally block is executing ");
+                Console.WriteLine("Age = " +age);
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
