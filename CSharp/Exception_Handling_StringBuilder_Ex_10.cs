500b486aff5406665b1507e2bf1309fa1592e45d
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 19:10:28 +0530

    Exception_Handling_StringBuilder_Ex_10

---
+++ CSharp/Exception_Handling_StringBuilder_Ex_10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_StringBuilder_Ex_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Dhananjay ");
+            sb.Append("Sonkusale");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("Bye");
+        }
+    }
+}
