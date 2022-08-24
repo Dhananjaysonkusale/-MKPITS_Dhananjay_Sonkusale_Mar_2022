4b1a12da89dc2721dde42b0012e2160293c1c2df
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:44:05 +0530

    Static_Ex_2

---
+++ CSharp/Static_Ex_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ex_2
+{
+    internal class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            display(); //to access static method display instance is not required
+        }
+    }
+}
