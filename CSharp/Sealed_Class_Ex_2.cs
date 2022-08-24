7a7c66fc2bc933ca71fcbba90b1305f9fcb052d0
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:51:03 +0530

    Sealed_Class_Ex_2

---
+++ CSharp/Sealed_Class_Ex_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Class_Ex_2
+{
+    internal class Program
+    {
+        abstract class person
+        {
+            public string name;
+        }
+        class student : person
+        {
+            public int rno;
+        }
+        sealed class parttimestudent : student
+        {
+            public int hours;
+        }
+        /*
+        class newstudent : parttimestudent //error
+
+        {
+
+        }
+        */
+        static void Main(string[] args)
+        {
+            // person p = new person(); //error
+            parttimestudent p = new parttimestudent();
+            p.rno = 22;
+            p.name = "Dhananjay";
+            p.hours = 5;
+            Console.WriteLine("Roll No: " + p.rno);
+            Console.WriteLine("Name : " + p.name);
+            Console.WriteLine("Hours : " + p.hours);
+        }
+    }
+}
