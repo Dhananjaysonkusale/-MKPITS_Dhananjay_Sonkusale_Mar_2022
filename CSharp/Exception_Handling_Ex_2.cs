b7afed60c28d70ec913397f91f68d9b6aa010692
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:56:34 +0530

    Exception_Handling_Ex_2

---
+++ CSharp/Exception_Handling_Ex_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, num;
+            Console.WriteLine("Enter Number 1st : ");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2nd : ");
+            n=Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            try
+            {
+                res = num / n; //error will come here and program terminates
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("Can not Divide By Zero ");
+            }
+            Console.WriteLine("Res = "+res);
+            Console.WriteLine("bye");
+
+        }
+    }
+}
