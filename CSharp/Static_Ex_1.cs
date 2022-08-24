ba49011858351319c14b607cbaf9fb26ba202c95
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:43:15 +0530

    Static_Ex_1

---
+++ CSharp/Static_Ex_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ex_1
+{
+    internal class Program
+    { 
+        //Static Keyword Example
+        void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            /*so p is an instance of class program and to access non static display method
+            we access it through p instance*/
+            p.display();
+        }
+    }
+}
