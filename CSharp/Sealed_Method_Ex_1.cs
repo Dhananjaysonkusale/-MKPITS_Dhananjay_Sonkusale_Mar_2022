a0d39525b6ba9936397dba41ff7c08ab6fb5f3a9
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:51:53 +0530

    Sealed_Method_Ex_1

---
+++ CSharp/Sealed_Method_Ex_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Method_Ex_1
+{
+    internal class Program
+    {
+        class person
+        {
+            public virtual void display()
+
+            {
+                Console.WriteLine("Hello from Class Person");
+            }
+        }
+        class student : person
+        {
+            public string name;
+            public sealed override void display()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+        class parttime : student
+        {
+            /*
+            public override void display() //error since sealed
+            {
+            Console.WriteLine("hello");
+            }
+            */
+        }
+        static void Main(string[] args)
+        {
+            parttime p = new parttime();
+            p.display();
+        }
+    }
+}
