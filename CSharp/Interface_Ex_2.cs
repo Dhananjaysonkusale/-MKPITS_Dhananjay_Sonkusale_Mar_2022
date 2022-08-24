c36e5324ec6347110b5f2fefa78993e66877de44
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:40:08 +0530

    Interface_Ex_2

---
+++ CSharp/Interface_Ex_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Ex_2
+{
+    internal class Program
+    {
+        interface bank
+        {
+            string deposit(int actno, int amt);
+        }
+        class saving : bank
+        {
+            int actno;
+            int bal = 1000;
+            public string deposit(int actno, int amt)
+            {
+                this.actno = actno;
+                bal = bal + amt;
+                return "Deposited successfully successfully , Bal is " + bal;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bank b;
+            b = new saving();
+            string str = b.deposit(1,200);
+            Console.WriteLine(str);
+        }
+    }
+}
