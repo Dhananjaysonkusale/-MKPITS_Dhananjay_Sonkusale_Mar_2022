baf20a20b5f74db6f670657b693dfd650003fd10
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:49:36 +0530

    Sealed_Class_Ex_1

---
+++ CSharp/Sealed_Class_Ex_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Class_Ex_1
+{
+    internal class Program
+    {
+        sealed public class Animal
+        {
+            public void eat() { Console.WriteLine("eating..."); }
+        }
+        public class Dog : Animal
+        {
+            public void bark() 
+            { 
+                Console.WriteLine("barking..."); 
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.eat();
+            d.bark();
+            //Compile Time Error: 'Dog': cannot derive from sealed type 'Animal'
+        }
+    }
+}
