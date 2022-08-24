cf497576980a542ba6762054afb42ec3d8bf4a15
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:31:04 +0530

    Interface_Ex_1

---
+++ CSharp/Interface_Ex_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Ex_1
+{
+    internal class Program
+    {
+        interface shape
+        {
+            void draw();
+
+        }
+        class rectangle : shape
+        {
+            public void draw()
+            {
+                Console.WriteLine("Draw method of Rectangl class");
+            }
+        }
+        class circle : shape
+
+        {
+            public void draw()
+            {
+                Console.WriteLine("Draw method of Circle class");
+            }
+        }
+        static void Main(string[] args)
+        {
+            shape s = new rectangle();
+            s.draw();
+            s = new circle();
+            s.draw();
+        }
+    }
+}
