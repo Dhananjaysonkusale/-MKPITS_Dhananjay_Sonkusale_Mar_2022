cfbe0416ab67f2df6954762ab757595c8950659a
Dhananjaysonkusale <101798903+Dhananjaysonkusale@users.noreply.github.com>
2022-08-24 18:45:28 +0530

    Static_Ex_3

---
+++ CSharp/Static_Ex_3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ex_3
+{
+    internal class Program
+    {
+        class student
+        {
+            public static void display()
+            {
+                Console.WriteLine(" Hello.......");
+            }
+            public void sayhi()
+            {
+                Console.WriteLine(" Hi......");
+            }
+        }
+        static void Main(string[] args)
+        {
+            student.display();
+            // student.sayhi();// error
+            student s = new student();
+            s.sayhi();
+        }
+    }
+}
